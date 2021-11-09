99d5734ea4d6dca59c57929aea2c5b8015f66c52
YiChengHsu <85992417+YiChengHsu@users.noreply.github.com>
2021-11-09 14:30:54 +0800

    Fix the signin bug

--- public/javascript/user_signin.js
+++ public/javascript/user_signin.js
@@ -17,31 +17,25 @@ form.addEventListener("submit", (e) => {
 		body: JSON.stringify(user),
 	})
 		.then((res) => {
-			if (res.status == 200) {
-				Swal.fire({
-					icon: 'success',
-					title: '登入成功',
-					text: '你可以隨便逛逛了唷~',
-				}).then(() => {
-					self.location.href = "/user/profile";
-				})
-			} else if (res.status == 403) {
+			if (res.status == 403) {
 				Swal.fire({
 					icon: 'error',
 					title: '登入失敗',
 					text: '請再試一次',
 				})
 				return;
-			} else {
-				alert("伺服器忙碌中，請稍後再試。");
-				return;
 			}
+			return res.json();
 		})
 		.then((res) => {
 			const data = res.data;
 			localStorage.setItem("user", JSON.stringify(data));
-			const user = JSON.parse(localStorage.getItem("user"));
-			console.log(user.user);
+			Swal.fire({
+				icon: 'success',
+				title: '登入成功',
+				text: '你可以隨便逛逛了唷~',
+			})
+			.then(() => {self.location.href = "/user/profile"})
 		})
 		.catch((err) => {
 			console.log(err);
