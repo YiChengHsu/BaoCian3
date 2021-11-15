908f8988e30b2b103a6ed557d7a8c95ce9794df5
YiChengHsu <85992417+YiChengHsu@users.noreply.github.com>
2021-11-15 17:03:03 +0800

    Fix the stripe webhook bug

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.engine("ejs", ejs.renderFile);
 
 // Use JSON parser for all non-webhook routes
 app.use((req, res, next) => {
-    if (req.originalUrl === '/api/1.0/order/webhook') {
+    if (req.originalUrl == 'https://www.baocian3.fun/api/1.0/order/webhook') {
       next();
     } else {
       express.json()(req, res, next);
